10a50910fd51a36f90b137494099cb45da872bd7
Jabel <jabel_fontoura@hotmail.com>
2017-03-16 18:56:00 -0300

    Define style fro headerText and thumbnail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
+  const { thumbnailStyle, headerTextStyle } = styles;
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
-      </CardSection>  
+        <View>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerTextStyle}>
+            <Text>{title}</Text>
+            <Text>{artist}</Text>
+        </View>
+      </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerTextStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  }
 };
 
 export default AlbumDetail;
