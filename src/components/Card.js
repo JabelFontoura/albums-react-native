a71fb9a6c857171359b484841188aea53be030e5
Jabel <jabel_fontoura@hotmail.com>
2017-03-16 00:32:05 -0300

    Create base Album component

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Card = () => {
+    return (
+      <View>
+
+      </View>
+    );
+};
+
+const styles = {
+  containerStyle: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#DDD',
+    boderBottomWidth: 0
+  }
+};
+
+export default Card;
