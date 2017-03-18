9d1456725642bb490feafeb472a3019a41066110
Jabel <jabel_fontoura@hotmail.com>
2017-03-17 21:07:09 -0300

    Create component Button

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,35 +4,60 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
-  const { thumbnailStyle, headerTextStyle } = styles;
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    headerTextStyle,
+    thumbnailContainerStyle,
+    imageStyle } = styles;
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             style={thumbnailStyle}
             source={{ uri: thumbnail_image }}
           />
         </View>
-        <View style={headerTextStyle}>
-            <Text>{title}</Text>
+        <View style={headerContentStyle}>
+            <Text style={headerTextStyle}>{title}</Text>
             <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }}
+        />
       </CardSection>
     </Card>
   );
 };
 
 const styles = {
-  headerTextStyle: {
+  headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
   },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
