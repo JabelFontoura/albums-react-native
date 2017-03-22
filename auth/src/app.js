57cf2e2234780120ea827f2dae4885bbf150b7ac
Jabel <jabel_fontoura@hotmail.com>
2017-03-21 21:59:27 -0300

    Structuring repository

---
+++ auth/src/app.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner, CardSection } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyD-ZnDolNsNoa7pgqdY5TKEN3_h0tlfQa4',
+      authDomain: 'auth-380be.firebaseapp.com',
+      databaseURL: 'https://auth-380be.firebaseio.com',
+      storageBucket: 'auth-380be.appspot.com',
+      messagingSenderId: '228576607803'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
+      </View>
+    );
+  }
+}
+
+export default App;
